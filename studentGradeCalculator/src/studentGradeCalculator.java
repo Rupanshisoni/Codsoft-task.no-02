8829b5dfb39cf70a0dc7876c1898239029c6409c
Rupanshisoni <rupanshirs67@gmail.com>
2024-12-21 10:59:52 +0530

    Add files via upload

---
+++ studentGradeCalculator/src/studentGradeCalculator.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+public class studentGradeCalculator {
+    public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Student Grade Calculator");
+                System.out.println("Enter number of subjects:");
+                int nS = sc.nextInt();
+                int total = 0;
+                for(int i=1; i<=nS;i++){
+                    System.out.println("Enter marks obtained in "+i+" : ");
+                    int marks = sc.nextInt();
+                    total += marks;
+                }
+                double averageP=(double)total/nS;
+                char Grade ;
+                if(averageP>=90){
+                    Grade='O';
+                }
+                else if(averageP >= 80){
+                    Grade = 'A';
+                }
+                else if(averageP >= 70){
+                    Grade = 'B';
+                }
+                else if(averageP >= 60){
+                    Grade = 'C';
+                }
+                else if(averageP >= 50){
+                    Grade = 'D';
+                }
+                else if(averageP >= 40){
+                    Grade = 'E';
+                }
+                else{
+                    Grade = 'F';
+                }
+                System.out.println("Total marks scored :"+total);
+                System.out.println("Average percentage gained :"+averageP+"%");
+                System.out.println("Grade :"+Grade);
+            }
+        }
+
+
+
